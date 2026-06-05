69ed1fb05124798d4b55cb8f93c5f5e0ae11ce12
marvinerbarth <marvinerbarth@gmail.com>
2026-06-05 21:42:55 +0200

    implemented shunting yard algorithm

--- expression-parser/expression-parser.cpp
+++ expression-parser/expression-parser.cpp
@@ -54,11 +54,53 @@ std::vector<Token> tokenize(std::string input) {
     return tokens;
 }
 
+int getPrecendence (Token t) {
+    switch (t.op) {
+        case'+':
+        case'-': {
+            return 1;
+        }
+        case'*':
+        case'/': {
+            return 2;
+        } // operator with higher precendence added  after here
+        default: return 0;
+    }
+}
+
+std::queue<Token> rpn(std::vector<Token> tokens) {
+    std::stack<Token> operat;
+    std::queue<Token> rpn;
+    for (int i = 0; i < tokens.size(); ++i) {
+        Token t = tokens[i];
+        if (t.type == 'N') rpn.push(t);
+        else if (t.type == 'O') {
+            if (operat.empty()) operat.push(t);
+            else {
+                while (!operat.empty() && getPrecendence(operat.top()) <= getPrecendence(t)) {
+                    rpn.push(operat.top());
+                    operat.pop();
+                }
+                operat.push(t);
+            }
+            
+        
+        }
+    }
+    while (!operat.empty()) {
+        rpn.push(operat.top());
+        operat.pop();
+    }  // new types added after here
+    return rpn;
+}
+
 int main()
 {
     std::string input{};
     std::cin >> input;
     std::vector<Token> tokens = tokenize(input); // only numbers from 1-9 and operators "*/+-" for version 1. 
+    std::queue<Token> rpnfunktion = rpn(tokens);
+      
     
 }
 
